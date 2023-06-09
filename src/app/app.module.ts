f3c718904ccf37bbe0550dd711adee357cbdb41a
grabu6 <mgrabulosa071@boscdelacoma.cat>
2023-06-09 15:12:01 +0200

    comencem examen

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,7 @@ import { AppComponent } from './app.component';
 import { CompatibilitiesComponent } from './components/compatibilities/compatibilities.component';
 import { HeroesComponent } from './components/heroes/heroes.component';
 import { SkillsComponent } from './components/skills/skills.component';
+import { ArmesComponent } from './components/armes/armes.component';
 
 @NgModule({
   declarations: [
@@ -13,6 +14,7 @@ import { SkillsComponent } from './components/skills/skills.component';
     CompatibilitiesComponent,
     HeroesComponent,
     SkillsComponent,
+    ArmesComponent,
   ],
   imports: [
     BrowserModule,
---
+++ src/app/components/armes/armes.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-armes',
+  templateUrl: './armes.component.html',
+  styleUrls: ['./armes.component.css']
+})
+export class ArmesComponent {
+
+}
